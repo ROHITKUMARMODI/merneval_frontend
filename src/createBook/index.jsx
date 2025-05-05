131eb840f94ec85478e6d8ea7bf9c9913c8752d8
ROHITKUMARMODI <modir8036@gmail.com>
2025-05-05 13:51:13 +0530

    Update index.jsx

--- src/createBook/index.jsx
+++ src/createBook/index.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
-
+import React,{useState} from 'react'
+import axios from "axios";
+import useNavigate from "react-router";
 export const CreateBook = () => {
+  const [author,setAuthor] = useState("")
+  const [Title,setTitle] = useState("")
+  const [price,setPrice] = useState()
+  const [page,setPages] = useState("")
+  const navigate=useNavigate();
+  let handleSubmit={
+    const apiObj={
+      title:title,
+      author:author,
+      price:price,
+      no of pages: page
+  }
+    axios({
+      method="GET",
+      URL="localhost:5173,
+      data:apiObj
+      )}.then((res)=> navigate("/books");
   return (
+    <>
     <div>CreateBook</div>
+    <input type="text" value={author} placeholder='Enter Author name' onChange={(e)=>setAuthor(e.target.value)}/> <br />
+    <input type="text" value={Title} placeholder='Enter Title name' onChange={(e)=>setTitle(e.target.value)}/> <br />
+    <input type="Number" value={price} placeholder='Enter Price' onChange={(e)=>setPrice(e.target.value)}/>
+   <br />
+    <input type="Number" value={Title} placeholder="Pages" onChange={(e)=>setPages(e.target.value)}/> <br />
+    <button onClick={()=>handleSubmit()}>Create Book</button>
+    </>
   )
 }
